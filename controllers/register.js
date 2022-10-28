8d46aab0cd16a06866853fd092f2d151f3a8c3ad
TeletubiGamin <emils.lecinskis@gmail.com>
2022-10-28 17:39:22 +0300

    a

--- controllers/register.js
+++ controllers/register.js
@@ -1,22 +1,34 @@
 const handleRegister = (req, res, db, bcrypt) => {
     
   const { email, password, username } = req.body;
+  if(!email || !username || !password){
+		return res.status(400).json('incorrect submition')
+	}
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
 
-  db('login').returning('email').insert({
-    email: email,
-    hash: hash
-  })
-  .then(
-      db('user').returning('email').insert({
-        email: email,
+  db.transaction.insert(trx => {
+    trx.insert({
+      email: email,
+      hash: hash
+    })
+    .into('login')
+    .returning('email')
+    .then(emails=>{ 
+      return trx('user')
+      .returning('email')
+      .insert({
+        email: emails[0].email,
         username: username,
         creation_date: new Date()
       })
-  )
-  .then(response=>res.json(response[0]))
-  .catch( res.json("cant"))
+      .then(response=>res.json(response[0]))
+      .catch( res.json("cant"))}
+    )
+    .then(trx.commit)
+    .catch(res.json("cant"))
+  })
+.catch(res.json("cant"))
 }
 
 module.exports={
