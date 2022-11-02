45b62e376560f5255cb5b137091babff051f73bf
TeletubiGamin <emils.lecinskis@gmail.com>
2022-11-02 21:14:02 +0200

    some new features

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,7 +4,6 @@ const handleTasks = (req, res, db) => {
    db.select()
     .where({theme_id: id})
       .table('task')
-        .orderBy('task_level')
           .orderBy('task_id')
    .then(resp =>res.json(resp))
     .catch(err=> res.json(err))
