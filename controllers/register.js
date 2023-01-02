d335d94f7c7eb19fdd10609d150eb48b85779082
TeletubiGamin <emils.lecinskis@gmail.com>
2023-01-02 15:06:14 +0200

    email fix

--- controllers/register.js
+++ controllers/register.js
@@ -4,10 +4,10 @@ const handleRegister = (req, res, db, bcrypt) => {
   if(!email || !username || !password) return res.status(400).json('incorrect submition')
 	
   const passwordRegex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')
-  const emailRegex = new RegExp('[^@\s]+@[^@ \t\r\n]+\.[^@ \t\r\n]+')
+  const emailRegex = new RegExp('[^@\s]+@[^@\s]+\.[^@\s]+')
   
-  if(!emailRegex.test(email)) return res.status(400).json('buddy this is not legally')
   if(!passwordRegex.test(password)) return res.status(400).json('buddy this is not legally2')
+  if(!emailRegex.test(email)) return res.status(400).json('buddy this is not legally')
   
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
