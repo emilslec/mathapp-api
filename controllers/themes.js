670e9b20f38b47e60f00973918a5bacb80019ddc
TeletubiGamin <emils.lecinskis@gmail.com>
2023-01-03 18:13:39 +0200

    task info

--- controllers/themes.js
+++ controllers/themes.js
@@ -4,6 +4,20 @@ const handleThemes = (req, res, db) => {
     .then(data => res.send(data))
 }
 
+const handleAddTheme = (req, res, db) => {
+  const {name, classs, key} = req.body;
+  if(!key==process.env.THEME_KEY) return res.json("not good do this")
+  db('theme')
+    .insert({
+      theme_class: classs,
+      theme_name: name
+    })
+    .returning('theme_id')
+    .then(resp=>res.json(resp))
+    .catch(err=>res.json(err))
+}
+
 module.exports = {
-    handleThemes
+    handleThemes,
+    handleAddTheme
 }
